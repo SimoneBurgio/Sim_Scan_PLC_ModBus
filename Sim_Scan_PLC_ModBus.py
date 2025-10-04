35f92da41351f7294dc5c613d8c21a37fd3e93ab
Simone Burgio <burgio.simone@gmail.com>
2025-10-04 13:14:38 +0200

    Creata struttura programma

--- Sim_Scan_PLC_ModBus.py
+++ Sim_Scan_PLC_ModBus.py
@@ -1,3 +1,83 @@
 import time
 import threading
 from pymodbus.client import ModbusTcpClient as MBClient
+
+#------------------------
+# -------- CONFIGURAZIONE 
+PLC_IP = "192.168.1.202"
+PLC_PORT = 502
+INPUT_REGISTER_ADDRESS = 32800
+
+#Indirizzi Modbus 4DO_P
+OUTPUT_1, OUTPUT_2, OUTPUT_3, OUTPUT_4 = 32768, 32769, 32770, 32771
+#Indirizzi Modbus 4DO_PN_2A
+OUTPUT_5, OUTPUT_6, OUTPUT_7, OUTPUT_8 = 32780, 32781, 32782, 32783
+
+utenti = {"Simone Burgio":  {"id": "0F00B18F42", "out": OUTPUT_1},
+          "Mario Roxy":     {"id": "909ABF4F", "out": OUTPUT_2 },
+          "Ganzo Frozo":    {"id": "AD87B1AB", "out": OUTPUT_3}
+          }
+
+# -------------------------------------------
+# ----------  DEFINIZIONE MEMORIE DI PROCESSO 
+pii = {"status1" : False,
+       "status2" : False,
+       "status3" : False}
+piq = {"lock1" : False,
+       "lock2" : False,
+       "lock3" : False,
+       "led"   : False}
+
+# -----------------------------------
+# ---------- SEZIONI LOGICA PROGRAMMA 
+def pou1(pii, piq):
+     if False in pii.values():
+          print("LED ACCESO")
+          piq["led"] = True
+     else:
+           piq["led"] = False
+
+
+          
+
+# --------------------------
+# ---------- TASK PRINCIPALE 
+
+def main_task():
+
+    with MBClient(PLC_IP, port=PLC_PORT) as client:
+        if not client.connect():
+            print(f"ERRORE: Connessione fallita con {PLC_IP}")
+        else:
+            print(f"Connessione con {PLC_IP} eseguita")
+    
+        while True:
+
+            # --- 1: LETTURA INGRESSI E SALVATAGGIO IN MEMORIA
+            try:
+                    result = client.read_input_registers(address=INPUT_REGISTER_ADDRESS, count=1)
+                    register_value = result.registers[0]
+
+                    pii["status1"] = (register_value & 1) != 0
+                    pii["status2"] = (register_value & 2) != 0
+                    pii["status3"] = (register_value & 4) != 0
+        
+            except Exception as e:
+                print(f"Si è verificato un errore durante la lettura: {e}") 
+            
+            # --- 2: ESECUZIONE LOGICA
+
+            pou1(pii,piq)
+
+
+
+            # --- 3: AGGIORNAMENTO USCITE
+
+            client.write_coil(OUTPUT_4, piq["led"])
+
+            time.sleep(0.1)
+
+
+
+if __name__ == "__main__":
+     main_task()
